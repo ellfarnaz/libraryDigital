539c7a85759d32b3d13a31aa620b1521784f6bb6
ellfarnaz <farelnaufalazhari@gmail.com>
2024-10-30 14:16:36 +0700

    coba 6

--- src/config/database.js
+++ src/config/database.js
@@ -2,13 +2,25 @@ const mysql = require("mysql2/promise");
 require("dotenv").config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQLPORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
+
+// Test koneksi
+pool
+  .getConnection()
+  .then((connection) => {
+    console.log("Database connected successfully");
+    connection.release();
+  })
+  .catch((err) => {
+    console.error("Error connecting to the database:", err);
+  });
 
 module.exports = pool;
